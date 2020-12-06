661727d06dd777b19ae2d5fe45c090a11ab5e452
B-Tran <btran054@ucr.edu>
2020-12-05 20:48:26 -0800

    Test made for Mock Functions Library
    - Mock_Functions Library works
    - Made test for the Mock Functions Library
    - Modified Mock_Functions header to output the values properly
    - Need to change the way that values are outputed
      currently hard coded into library, make it more generalized
    - Need to test mock servo library

--- Tests/Mock_Libraries/Mock_Functions.h
+++ Tests/Mock_Libraries/Mock_Functions.h
@@ -2,11 +2,12 @@
 #define MOCK_FUNCTIONS_H
 
 #include <iostream>
-
+#include <vector>
+#include <string>
 //This header contains the mock functionality of the general purpose input output pins
 
-enum {INPUT, OUTPUT} pin_type; //this defines the pin as set to input or output
-enum {LOW, HIGH} pin_out; //defines if output is high or low
+enum pin_type{INPUT, OUTPUT}; //this defines the pin as set to input or output
+enum pin_out{LOW, HIGH}; //defines if output is high or low
 
 #define D0 0
 #define D1 1
@@ -29,30 +30,26 @@ enum {LOW, HIGH} pin_out; //defines if output is high or low
 #define A4 18
 #define A5 19
 
+//output vector of the results
+std::vector<std::string> output;
 //gpio read pin values
-int pinD0;
-int pinD1;
-int pinD2;
-int pinD3;
-int pinD4;
-int pinD5;
-int pinD6;
-int pinD7;
-int pinD8;
-int pinD9;
-int pinD10;
-int pinD11;
-int pinD12;
-int pinD13;
-int pinA0;
-int pinA1;
-int pinA2;
-int pinA3;
-int pinA4;
-int pinA5;
+char pinIO[19];
 
 //"sets" pinmode
 void pinMode(const int & pin, const pin_type & gpio)
+{
+    if(gpio == INPUT)
+    {
+        std::cout<< "Setting pin: " << pin << " as INPUT\n";
+    }
+    else
+    {
+        std::cout<< "Setting pin: " << pin << " as OUTPUT\n";
+    }
+}
+
+//"sets" pinmode
+void pinMode(const int & pin, const int & gpio)
 {
     if(gpio == INPUT)
     {
@@ -69,119 +66,27 @@ void pinMode(const int & pin, const pin_type & gpio)
 //calling this function
 char digitalRead(const char & pin)
 {
-    switch(pin)
+    if(pin >= 0 && pin <=19)
     {
-        case 0:
-        {
-            return pinD0;
-            break;
-        }
-        case 1:
-        {
-            return pinD1;
-            break;
-        }
-        case 2:
-        {
-            return pinD2;
-            break;
-        }
-        case 3:
-        {
-            return pinD3;
-            break;
-        }
-        case 4:
-        {
-            return pinD4;
-            break;
-        }
-        case 5:
-        {
-            return pinD5;
-            break;
-        }
-        case 6:
-        {
-            return pinD6;
-            break;
-        }
-        case 7:
-        {
-            return pinD7;
-            break;
-        }
-        case 8:
-        {
-            return pinD8;
-            break;
-        }
-        case 9:
-        {
-            return pinD9;
-            break;
-        }
-        case 10:
-        {
-            return pinD10;
-            break;
-        }
-        case 11:
-        {
-            return pinD11;
-            break;
-        }
-        case 12:
-        {
-            return pinD12;
-            break;
-        }
-        case 13:
-        {
-            return pinD13;
-            break;
-        }
-        case 14:
-        {
-            return pinA0;
-            break;
-        }
-        case 15:
-        {
-            return pinA1;
-            break;
-        }
-        case 16:
-        {
-            return pinA2;
-            break;
-        }
-        case 17:
-        {
-            return pinA3;
-            break;
-        }
-        case 18:
-        {
-            return pinA4;
-            break;
-        }
-        case 19:
-        {
-            return pinA5;
-            break;
-        }
-        default:
-        {
-            throw "INVALID PIN\n";
-            break;
-        }
+        return pinIO[pin];
+    }
+    else
+    {
+        throw "INVALID PIN\n";
     }
 }
 
 //"writes" data to the pin
-void digitalWrite(const char & pin, bool value)
+void digitalWrite(const char & pin, const bool & value)
 {
-    std::cout<< "Setting pin: " << pin << " to " << value << "\n";
+    output.push_back("Writing to pin " + std::to_string(pin) + ": " + std::to_string(value));
+    pinIO[pin] = value;
+}
+
+//"writes" data to the pin
+void digitalWrite(const char & pin, const pin_out & value)   
+{
+    output.push_back("Writing to pin " + std::to_string(pin) + ": " + std::to_string(value));
+    pinIO[pin] = value;
 }
 #endif
---
+++ Tests/Test_Files/GPIO_Test.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "../Mock_Libraries/Mock_Functions.h"
+#include "../Mock_Libraries/Mock_Time.h"
+
+#define LED_BUILTIN 13 //defining led_builtin for blink example test
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+    static unsigned long start = 0;
+    const unsigned long blink_Delay = 1000;
+    static char x;
+
+    if( (millis() - start) > blink_Delay)
+    {
+        x = digitalRead(LED_BUILTIN);
+        if(x == 1)
+        {
+            digitalWrite(LED_BUILTIN, LOW);
+        }
+        else
+        {
+            digitalWrite(LED_BUILTIN, HIGH);
+        }
+        start = millis();
+    }
+}
+
+int main()
+{
+    //this test is to see if the Mock_Functions library works properly
+    //set the "builtin" led initially to low
+    pinIO[LED_BUILTIN] = 0;
+
+    //perform "setup"
+    setup();
+
+    //initialize the timer then run the program for 6 seconds
+    unsigned long finish = 6001;
+    initialize_time();
+    unsigned long start = millis();
+    while( (millis() - start) <= finish)
+    {
+        loop();
+    }
+    //expecting results
+    std::cout<< "Expected results: writing out to pin " << LED_BUILTIN << ": 1 0 1 0 1 0 \n";
+    std::cout << "Actual results : \n";
+    //prints the results
+    for(int i = 0; i < output.size(); ++i)
+    {
+        std::cout << output.at(i) << "\n";
+    }
+}
