90f2db963a9daaf71d9a61365225ae367f502d0f
Sameh Fazli <43709736+sfazli96@users.noreply.github.com>
2020-12-17 09:21:31 -0800

    Updated detect_face.py, erased commented code

--- Face_RecognitionV1/detect_face.py
+++ Face_RecognitionV1/detect_face.py
@@ -79,21 +79,3 @@ while True:
         break
 ## Press escape to exit the program
 cap.release()
-
-
-'''
-## This will do the recognition for the face, so Facial recognition
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-img = cv2.imread('group.png')
-
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-for (x, y, w, h) in faces:  
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0)) 
-
-cv2.imshow('img', img)
-cv2.waitKey()
-'''
