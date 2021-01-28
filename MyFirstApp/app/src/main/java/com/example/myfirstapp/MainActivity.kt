cfe5519a44c2b90cc530219e51c3c589a2309806
B-Tran <btran054@ucr.edu>
2021-01-27 18:18:42 -0800

    Updated Application to Create Push Button Notification
    - Creates notification on button press
    - Still needs to allow for multiple notifications
    - Needs expanding notifications
    - Need to convert to event notification

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.myfirstapp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
@@ -14,6 +21,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    /*val textTitle = "Test Notification"
+    var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.Test_Notification)
+            .setContentTitle(textTitle)
+            .setContentText(EXTRA_MESSAGE)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)*/
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
         // Do something in response to button
@@ -24,4 +37,49 @@ class MainActivity : AppCompatActivity() {
         }
         startActivity(intent)
     }
+
+    fun sendMessage2(view: View)
+    {
+        val title = "Test Notification"
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("YOUR_CHANNEL_ID",
+                    "YOUR_CHANNEL_NAME",
+                    NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = "YOUR_NOTIFICATION_CHANNEL_DESCRIPTION"
+            mNotificationManager.createNotificationChannel(channel)
+        }
+        val mBuilder = NotificationCompat.Builder(applicationContext, "YOUR_CHANNEL_ID")
+                .setSmallIcon(R.mipmap.ic_launcher) // notification icon
+                .setContentTitle(title) // title for notification
+                .setContentText(message)// message for notification
+                .setAutoCancel(true) // clear notification after click
+        val intent = Intent(applicationContext, DisplayMessageActivity::class.java)
+        val pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        mBuilder.setContentIntent(pi)
+        mNotificationManager.notify(0, mBuilder.build())
+    }
+
+    /*fun showNotification(title: String, message: String) {
+        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("YOUR_CHANNEL_ID",
+                    "YOUR_CHANNEL_NAME",
+                    NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = "YOUR_NOTIFICATION_CHANNEL_DESCRIPTION"
+            mNotificationManager.createNotificationChannel(channel)
+        }
+        val mBuilder = NotificationCompat.Builder(applicationContext, "YOUR_CHANNEL_ID")
+                .setSmallIcon(R.mipmap.ic_launcher) // notification icon
+                .setContentTitle(title) // title for notification
+                .setContentText(message)// message for notification
+                .setAutoCancel(true) // clear notification after click
+        val intent = Intent(applicationContext, DisplayMessageActivity::class.java)
+        val pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        mBuilder.setContentIntent(pi)
+        mNotificationManager.notify(0, mBuilder.build())
+    }
+    */
 }
