8982ba0fcb976596a7d38abe55c232e8d222f7bb
LC <luccas.chang@gmail.com>
2021-02-03 01:11:43 -0800

    added custom video player; atm depends on hardcoded url that holds a video of some 80s action cartoon; will possibly necessitate extensive modifications for network usage or possible a total rework

--- cs178_MyFirstApp/app/src/main/java/com/example/cs178_myfirstapp/video_activity.java
+++ cs178_MyFirstApp/app/src/main/java/com/example/cs178_myfirstapp/video_activity.java
@@ -2,13 +2,192 @@ package com.example.cs178_myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 
 public class video_activity extends AppCompatActivity {
+    //init variable
+
+    PlayerView playerView;
+    ProgressBar progressBar;
+    ImageView btFullScreen;
+
+    SimpleExoPlayer simpleExoPlayer;
+    boolean flag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_activity);
+
+        //assign vars
+        playerView = findViewById(R.id.player_view);
+        progressBar = findViewById(R.id.progress_bar);
+        btFullScreen = playerView.findViewById(R.id.bt_fullscreen);
+
+        //make activity fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //vid url
+        Uri videoUrl = Uri.parse("https://imgur.com/7bMqysJ.mp4");
+
+        //init load ctrl
+        LoadControl loadControl = new DefaultLoadControl();
+        //init band widtch meter
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        //init track selector
+        TrackSelector trackSelector = new DefaultTrackSelector(
+                new AdaptiveTrackSelection.Factory(bandwidthMeter)
+        );
+        //init simple exo player
+        simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(
+                video_activity.this, trackSelector, loadControl
+        );
+        //init data source factory
+        DefaultHttpDataSourceFactory factory = new DefaultHttpDataSourceFactory(
+                "exoplayer_video"
+        );
+        //init extractors factory
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+        //init media source
+        MediaSource mediaSource = new ExtractorMediaSource(videoUrl
+                , factory, extractorsFactory, null, null
+        );
+        //set player
+        playerView.setPlayer(simpleExoPlayer);
+        //keep screen on
+        playerView.setKeepScreenOn(true);
+        //prep media
+        simpleExoPlayer.prepare(mediaSource);
+        //play vid when ready
+        simpleExoPlayer.setPlayWhenReady(true);
+        simpleExoPlayer.addListener(new Player.EventListener() {
+            @Override
+            public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+
+            }
+
+            @Override
+            public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+            }
+
+            @Override
+            public void onLoadingChanged(boolean isLoading) {
+
+            }
+
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                if (playbackState == Player.STATE_BUFFERING) {
+                    //show prog when buffering
+                    progressBar.setVisibility(View.VISIBLE);
+                } else if (playbackState == Player.STATE_READY) {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onRepeatModeChanged(int repeatMode) {
+
+            }
+
+            @Override
+            public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+            }
+
+            @Override
+            public void onPlayerError(ExoPlaybackException error) {
+
+            }
+
+            @Override
+            public void onPositionDiscontinuity(int reason) {
+
+            }
+
+            @Override
+            public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+            }
+
+            @Override
+            public void onSeekProcessed() {
+
+            }
+        });
+
+        btFullScreen.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SourceLockedOrientationActivity")
+            @Override
+            public void onClick(View view) {
+                //check condition
+                if (flag) {
+                    //when flag is true, set enter fullscreen img
+                    btFullScreen.setImageDrawable(getResources().getDrawable(R.drawable.ic_fullscreen));
+                    //set portrait orientation
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+                    //set flag value is false
+                    flag = false;
+                } else {
+                    //if flag is false, set exit fullscreen img
+                    btFullScreen.setImageDrawable(getResources().getDrawable(
+                            R.drawable.ic_fullscreen_exit
+                    ));
+                    //set landscape
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                    flag = true;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //stop video when ready
+        simpleExoPlayer.setPlayWhenReady(false);
+        //get playaback state
+        simpleExoPlayer.getPlaybackState();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        //play vid when ready
+        simpleExoPlayer.setPlayWhenReady(true);
+        //get playback state
+        simpleExoPlayer.getPlaybackState();
     }
 }
