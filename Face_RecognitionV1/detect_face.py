c7ed2e56987769ab28cda7b28cb331f1b950556a
Eduardo Rocha <eduardor626@yahoo.com>
2021-02-04 13:05:20 -0800

    messing with .py to fix rectangle

--- Face_RecognitionV1/detect_face.py
+++ Face_RecognitionV1/detect_face.py
@@ -1,13 +1,13 @@
 import numpy as np
 import cv2
 import time
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(23,GPIO.OUT)
-GPIO.setup(24,GPIO.OUT)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setwarnings(False)
+#GPIO.setup(18,GPIO.OUT)
+#GPIO.setup(23,GPIO.OUT)
+#GPIO.setup(24,GPIO.OUT)
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
 
@@ -38,27 +38,27 @@ while True:
         #if x1 >= 250 and x2 <= 500:
            # print("[INFO] found {0} faces!".format(len(faces)))
            # GPIO.output(18,GPIO.HIGH)
-        if x1 < 350: # If our x coordinates is less than 225, then we move our face more left to the center, so  our face gets recognize
+        if x1 < 200: # If our x coordinates is less than 225, then we move our face more left to the center, so  our face gets recognize
             print("move left")
-            GPIO.output(18,GPIO.LOW)
-            GPIO.output(23,GPIO.HIGH)
-            GPIO.output(24,GPIO.LOW)
+#            GPIO.output(18,GPIO.LOW)
+#            GPIO.output(23,GPIO.HIGH)
+#            GPIO.output(24,GPIO.LOW)
 
-        elif x2 > 550: #if our x coordinates is greater than 475, then we move our face more right to the center, so our face gets recognize
+        elif x2 > 600: #if our x coordinates is greater than 475, then we move our face more right to the center, so our face gets recognize
             print("move right")
-            GPIO.output(18,GPIO.LOW)
-            GPIO.output(23,GPIO.LOW)
-            GPIO.output(24,GPIO.HIGH)
+#            GPIO.output(18,GPIO.LOW)
+#            GPIO.output(23,GPIO.LOW)
+#            GPIO.output(24,GPIO.HIGH)
         else:
             print("[INFO] found {0} faces!".format(len(faces))) #Now, we are in the center of the camera, face detected, now shoot.
-            GPIO.output(18,GPIO.HIGH)
-            GPIO.output(23,GPIO.LOW)
-            GPIO.output(24,GPIO.LOW)
+#            GPIO.output(18,GPIO.HIGH)
+#            GPIO.output(23,GPIO.LOW)
+#            GPIO.output(24,GPIO.LOW)
     else:
         print("No face") #No person is in scope of the camera so turn off everything
-        GPIO.output(18,GPIO.LOW)
-        GPIO.output(23,GPIO.LOW)
-        GPIO.output(24,GPIO.LOW)
+#        GPIO.output(18,GPIO.LOW)
+#        GPIO.output(23,GPIO.LOW)
+#        GPIO.output(24,GPIO.LOW)
 
     curTime = time.time()
     sec = curTime - prevTime
@@ -75,4 +75,4 @@ while True:
     if x==27:
         break
 ## Press escape to exit the program
-cap.release()g
+cap.release()
