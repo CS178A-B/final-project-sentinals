84880b5856d58179a7ca38503c5c3930f655ac1b
B-Tran <btran054@ucr.edu>
2021-02-10 20:50:29 -0800

    Application Can Now Display Multiple Notifications

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.myfirstapp
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
+import android.app.*
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -38,12 +37,23 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+
+
     fun sendMessage2(view: View)
     {
-        val title = "Test Notification"
+        // this is a temporary file that holds the channel value
+        val sharedPref = getSharedPreferences(getString(R.string.preference_file_key) , Context.MODE_PRIVATE)
+        // this is the notification channel number
+        var notificationNumber = sharedPref.getInt("notificationNumber", 0);
+        // title of the notification
+        val title = "Sentry Alert"
+        // temp value to get the text from the message box
         val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        // converting the text into the notification message
         val message = editText.text.toString()
+        //creating the notification mannager to intialize notificatons
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        //creating a channel for android version 7.0+
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             val channel = NotificationChannel("YOUR_CHANNEL_ID",
                     "YOUR_CHANNEL_NAME",
@@ -51,15 +61,27 @@ class MainActivity : AppCompatActivity() {
             channel.description = "YOUR_NOTIFICATION_CHANNEL_DESCRIPTION"
             mNotificationManager.createNotificationChannel(channel)
         }
+        // building the notification
         val mBuilder = NotificationCompat.Builder(applicationContext, "YOUR_CHANNEL_ID")
                 .setSmallIcon(R.mipmap.ic_launcher) // notification icon
                 .setContentTitle(title) // title for notification
                 .setContentText(message)// message for notification
                 .setAutoCancel(true) // clear notification after click
-        val intent = Intent(applicationContext, DisplayMessageActivity::class.java)
+        // giving the action that will happen when the user presses the notfication
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        // defining pending intent for the notification to wait for an action
         val pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        // assigning the intent to the notification
         mBuilder.setContentIntent(pi)
-        mNotificationManager.notify(0, mBuilder.build())
+        //sending the notification on the channel number
+        mNotificationManager.notify( notificationNumber, mBuilder.build())
+        //updating the notification channel number to a different value to allow for multiple notifications
+        val editor = sharedPref.edit()
+        notificationNumber++
+        editor.putInt("notificationNumber", notificationNumber)
+        editor.apply()
+
+
     }
 
     /*fun showNotification(title: String, message: String) {
