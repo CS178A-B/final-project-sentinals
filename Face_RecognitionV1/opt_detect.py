3eac829505115d824b312a580ff49aff73a96db6
Eduardo Rocha <eduardor626@yahoo.com>
2021-02-11 17:07:28 -0800

    fixed coordinate issues

--- Face_RecognitionV1/opt_detect.py
+++ Face_RecognitionV1/opt_detect.py
@@ -2,13 +2,13 @@ import numpy as np
 import cv2
 import time
 import imutils
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setwarnings(False)
-#GPIO.setup(18,GPIO.OUT)
-#GPIO.setup(23,GPIO.OUT)
-#GPIO.setup(24,GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(24,GPIO.OUT)
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
 
@@ -47,32 +47,32 @@ while True:
         #top Left corner of square 
         print("diagonal point 1(x1, y1) = ({},{})".format(x1, y1)) 
         #top right and bottom left corners of square
-        print("diagonal point 2(x2, y2) = ({},{})".format(x2, y2)) # This would be top right corner
+        print("diagonal point 2(x2, y2) = ({},{})".format(x2, y2)) 
     if len(faces) > 0:
-        if x1 < 200: # person is  
-            print("move LEFT")
-#           GPIO.output(18,GPIO.LOW)
- #          GPIO.output(23,GPIO.HIGH)
-#           GPIO.output(24,GPIO.LOW)
+        if x1 < 400: # person is  
+           print("move LEFT")
+           GPIO.output(18,GPIO.LOW)
+           GPIO.output(23,GPIO.HIGH)
+           GPIO.output(24,GPIO.LOW)
 
         elif x2 > 600: # person is 
             print("move RIGHT")
-#            GPIO.output(18,GPIO.LOW)
-#            GPIO.output(23,GPIO.LOW)
-#            GPIO.output(24,GPIO.HIGH)
+            GPIO.output(18,GPIO.LOW)
+            GPIO.output(23,GPIO.LOW)
+            GPIO.output(24,GPIO.HIGH)
         else:
             #print("[INFO] found {0} faces!".format(len(faces))) #Now, we are in the center of the camera, face detected, now shoot.
             #Now, we are in the center of the camera, face detected, now shoot.
             print("FIRE! FIRE! FIRE!") 
 
-#            GPIO.output(18,GPIO.HIGH)
-#            GPIO.output(23,GPIO.HIGH)
-#            GPIO.output(24,GPIO.HIGH)
+            GPIO.output(18,GPIO.HIGH)
+            GPIO.output(23,GPIO.HIGH)
+            GPIO.output(24,GPIO.HIGH)
     else:
         print("No face") #No person is in scope of the camera so turn off everything
-#        GPIO.output(18,GPIO.LOW)
-#        GPIO.output(23,GPIO.LOW)
-#        GPIO.output(24,GPIO.LOW)
+        GPIO.output(18,GPIO.LOW)
+        GPIO.output(23,GPIO.LOW)
+        GPIO.output(24,GPIO.LOW)
 
     curTime = time.time()
     sec = curTime - prevTime
