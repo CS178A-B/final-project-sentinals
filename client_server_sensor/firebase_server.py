57a704e980ee27754671f5fbe6123d9cbbfb5fce
LC <luccas.chang@gmail.com>
2021-03-08 21:51:27 -0800

    cleanup android app; app itself no longer has any particular features and now serves simply to receive notifications from Firebase

--- client_server_sensor/firebase_server.py
+++ client_server_sensor/firebase_server.py
@@ -4,26 +4,28 @@
 #default_app = firebase_admin.initialize_app(cred)
 #print(default_app.name)
 
-
+#To find the token to your device, upon installating the companion_app to your device,
+#check the debug console on android studio and look for "D/TAG: the token refreshed: ...". 
+#The following string is your device token, take that and assign it to this file's registration_token variable.
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import messaging
 #import firebase_messaging
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("<insert path to json file>")
+    cred = credentials.Certificate("E:\Luccas\Documents\docs_2\UCR Docs\Fall_2020\cs178a\cs178b-notification-firebase-adminsdk-4vs8i-c3e54116ce.json")
     firebase_admin.initialize_app(cred)
 
 #default_app = firebase_admin.initialize_app()
 
 # This registration token comes from the client FCM SDKs.
-registration_token = '<token for device>'
+registration_token = 'cHxxQeyDRFCkB5HrLzs8yx:APA91bGHRtn-eDOZfO4dKp7fHEL_uNIydJmi7-KJN5ScpfKKaAsdwhvznqDShgeAOcBVSIPBjbnwFv2tk_w-bqq49IW9Lx_rjtKewRtN2-HjK-zk5UQd2S5bx8H_63Kp134uyk1B_b2O'
 
 # See documentation on defining a message payload.
 message = messaging.Message(
     notification=messaging.Notification(
-        title='hello there',
-        body='general kenobi! you are a bold one!'
+        title='ALERT FROM NERF SENTRY',
+        body='Your Nerf Sentry detected a possible intruder and was signaled to fire.'
     ),
     #data={
     #    'score': '850',
--- cs178_MyFirstApp/app/src/main/java/com/example/cs178_myfirstapp/MainActivity.java
+++ cs178_MyFirstApp/app/src/main/java/com/example/cs178_myfirstapp/MainActivity.java
@@ -11,81 +11,14 @@ import android.widget.EditText;
 
 
 
-/*public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}*/
-
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.cs128_MyFirstApp.MESSAGE";
-    private Button vid_stream;
-    private Button tcp_sample;
-
-    /*String SENDER_ID = "";
-    if (regid.isEmpty()) {
-        registerInBackground();
-    }else{
-        Log.e("==========================","=========================");
-        Log.e("regid",regid);
-        Log.e("==========================","=========================");
-    } */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        vid_stream = findViewById(R.id.video_stream);
-        vid_stream.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openVidStream();
-            }
-        });
-
-        tcp_sample = findViewById(R.id.tcp);
-        tcp_sample.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openTCP();
-            }
-        });
     }
-
-    //called when user taps send button
-    public void sendMessage(View view) {
-        //do something
-        //intent - obj that provides runtime binding bw separate components like two activities
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        //above's second arg is the activity, or 'intent', to start
-
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-
-        //
-        intent.putExtra(EXTRA_MESSAGE, message);
-        //basically starts the displaymessageactivity "string"
-        startActivity(intent);
-    }
-
-    public void videoActivity(View v) {
-        Intent i = new Intent(this, video_activity.class);
-        startActivity(i);
-    }
-
-    public void openVidStream() {
-        Intent intent = new Intent(this, video_stream.class);
-        startActivity(intent);
-    }
-
-    public void openTCP() {
-        Intent intent = new Intent(this, TCP.class);
-        startActivity(intent);
-    }
-
 
 }
