0104b70868bd38931bf5917e8ad5b54a21fc8c43
Sameh Fazli <sfazli96@gmail.com>
2021-03-09 19:25:47 -0800

    Cleaned up old code

--- Face_RecognitionV1/server.py
+++ Face_RecognitionV1/server.py
@@ -19,15 +19,15 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 class IndexHandler(web.RequestHandler):
-    """ Handler for the root static page. """
+    """ This is our Handler for the root static page. """
     def get(self):
         self.render('index.html')
 
 class SocketHandler(websocket.WebSocketHandler):
-    """ Handler for the websocket URL. """
+    """ This is our Handler for the websocket URL. """
     
     def __init__(self, *args, **kwargs):
-        """ Initialize the Redis store and framerate monitor. """
+        """ We do this to Initialize the Redis store and framerate monitor. """
 
         super(SocketHandler, self).__init__(*args, **kwargs)
         self._store = redis.Redis()
@@ -49,22 +49,13 @@ class SocketHandler(websocket.WebSocketHandler):
         self._prev_image_id = image_id
         image_byte = self._store.get('image')
         image = np.frombuffer(image_byte, dtype=np.uint8)  ## Interpret a buffer as a 1-dimensional array / turns into np array.
-        images_gray = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE) 
-        '''
-        image = image.reshape(len(image), 1)  # Gives a new shape to an array without changing its data.
-       # print(df.shape)
-        coeffs = np.array([0.114, 0.587, 0.229])  ## Creates an array
-        coeffs = coeffs.reshape(1, 3)   # Modifying/reshaping the matrix but keeping the same coefficients
-       # print(coeffs.shape)
-        images_gray = np.matmul(image.astype(np.float), coeffs).sum(axis=-1)  ## RGB to grayscale for the opencv
-        images_gray = images_gray.astype(np.uint8)
-        '''
+        images_gray = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
+        
             # This is required for opencv. Face recognition code below.
         faces = face_cascade.detectMultiScale(images_gray, 1.1, 5) ## Next, we detect the faces
 
         for (x, y, w, h) in faces:  # This will find our coordinates and y
             x1 = x
-            #self._store.set('coor', x)
             x2 = x + w
             y1 = y
             y2 = y + h
@@ -78,41 +69,17 @@ class SocketHandler(websocket.WebSocketHandler):
                 # GPIO.output(18,GPIO.HIGH)
             if x1 < 200: # If our x coordinates is less than 225, then we move our face more left to the center, so  our face gets recognize
                 self._store.set('move_position', "move left")
-            #          GPIO.output(18,GPIO.LOW)
-            #          GPIO.output(23,GPIO.HIGH)
-            #          GPIO.output(24,GPIO.LOW)
-
             elif x2 > 400: #if our x coordinates is greater than 475, then we move our face more right to the center, so our face gets recognize
                 self._store.set('move_position', "move right")
-            #          GPIO.output(18,GPIO.LOW)
-            #          GPIO.output(23,GPIO.LOW)
-            #          GPIO.output(24,GPIO.HIGH)
             else:
                 self._store.set('move_position', "found faces")
-
-            #          GPIO.output(18,GPIO.HIGH)
-            #          GPIO.output(23,GPIO.LOW)
-            #          GPIO.output(24,GPIO.LOW)
         else:
             self._store.set('move_position', "No Faces")
 
-            #      GPIO.output(18,GPIO.LOW)
-            #      GPIO.output(23,GPIO.LOW)
-            #      GPIO.output(24,GPIO.LOW)
-
-        #    curTime = time.time()
-        #    sec = curTime - prevTime
-        #    prevTime = curTime
-        #    fps = 1/(sec)
-        #    str_1 = "FPS : %0.1f" % fps 
         for (x, y, w, h) in faces:   ## We draw a rectangle around the faces so we can see it correctly
             cv2.rectangle(images_gray, (x, y), (x+w, y+h), (255, 0, 0))         ## The faces will be a list of coordinates
             cv2.putText(images_gray, 'Myface', (x, y), font, fontScale=1, color=(255,70,120),thickness=2)
-       # cv2.putText(image, 'Number of Faces Detected: ' + str, (0,  100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
-        #cv2.imshow('img', frame) ## Last we show the image
-
-
-       # x = cv2.waitKey(30) & 0xff
+    
         retval, frame = cv2.imencode('.jpg', images_gray) # frame is memory buffer of jpg image
         value = np.array(frame).tobytes()
         image_byte = base64.b64encode(value)
