d07afabd899016564b3eb6a4378b3ba2322b8662
LC <luccas.chang@gmail.com>
2021-03-10 14:03:43 -0800

    added flag in client.py to reduce notification spamming

--- Face_RecognitionV1/client.py
+++ Face_RecognitionV1/client.py
@@ -14,7 +14,7 @@ GPIO.setup(23,GPIO.OUT)
 GPIO.setup(24,GPIO.OUT)
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
-noti_flag = 0
+notif_flag = 0
 prevTime = 0
 ## This will get our web camera 
 #cap = cv2.VideoCapture(0)
@@ -78,23 +78,28 @@ while True:
            # GPIO.output(18,GPIO.HIGH)
         if image_position == 'move left': # If our x coordinates is less than 225, then we move our face more left to the center, so  our face gets recognize
             print("move left")
+            notif_flag = 0
             GPIO.output(18,GPIO.LOW)
             GPIO.output(23,GPIO.HIGH)
             GPIO.output(24,GPIO.LOW)
 
         elif image_position == 'move right': #if our x coordinates is greater than 475, then we move our face more right to the center, so our face gets recognize
             print("move right")
+            notif_flag = 0
             GPIO.output(18,GPIO.LOW)
             GPIO.output(23,GPIO.LOW)
             GPIO.output(24,GPIO.HIGH)
         elif image_position == 'found faces':
             print("FIRE!!!") #Now, we are in the center of the camera, face detected, now shoot.
-            exec(open('firebase_server.py').read())
+            if notif_flag == 0:
+                exec(open('firebase_server.py').read())
+                notif_flag = 1
             GPIO.output(18,GPIO.HIGH)
             GPIO.output(23,GPIO.LOW)
             GPIO.output(24,GPIO.LOW)
         else:
             print("No face") #No person is in scope of the camera so turn off everything
+            notif_flag = 0
             GPIO.output(18,GPIO.LOW)
             GPIO.output(23,GPIO.LOW)
             GPIO.output(24,GPIO.LOW)
